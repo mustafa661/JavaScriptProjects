ffa46fe1a2188dd6011710964ad07c1005cac033
mustafa <mustafashabbir804@gmail.com>
2021-06-16 22:56:57 +0500

    Update script.js
    Added email validation with simple JS

--- p01/script.js
+++ p01/script.js
@@ -19,6 +19,12 @@ function showsuccess(input) {
     formControl.className = 'form-control success';
 }
 
+// Function to check if email is valid
+function isvalidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 // This is an event listener for the form on submit 
 form.addEventListener('submit',function(e) {
     e.preventDefault();
@@ -31,6 +37,8 @@ form.addEventListener('submit',function(e) {
     
     if ( email.value === '' ) {
         showError(email,'Email is required')
+    } else if (!isvalidEmail(email.value)) {
+        showError(email,'Email is invalid')
     } else {
         showsuccess(email);
     }
